d22534c0e487f7f5df4e145416173749d6665b62
jencolletta <jencolletta@gmail.com>
2023-12-01 15:44:14 -0500

    Fix nextjs.yml error

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: '/nextjs-github-pages',
+  basePath: '/google-search',
 
   /**
    * Disable server-based image optimization. Next.js does not support
